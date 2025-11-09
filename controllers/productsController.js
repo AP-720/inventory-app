8fb0d5834438e07a9c21aed2fc9fb4ab6061a899
Andy Price <andyprice720@gmail.com>
2025-11-09 21:30:06 +0000

    feat(productCard): create basic product card
    - Create productCard partial
    - Add hardcoded test data to "productsController.js"

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,8 +3,24 @@ const db = require("../db/queries");
 async function getAllProducts(req, res) {
 	try {
 		// const products = await db.getAllProducts();
+		const products = [
+			{
+				id: 1,
+				name: "La Esperanza",
+				roaster_name: "Crank House Coffee",
+				roaster_country: "UK",
+				origin_region: "Huehuetenango",
+				origin_country: "Guatemala",
+				process: "Washed",
+				variety: "Caturra, Bourbon, Catuai, Pache",
+				tasting_notes: "Gooseberry, red apple, caramel",
+				roast_style: "Light",
+				roast_type: "Filter",
+				price: 20,
+			},
+		];
 
-		res.render("products", { title: "Products" });
+		res.render("products", { title: "Products", products });
 	} catch (err) {
 		console.error(err);
 		res.status(500).send("Server error");

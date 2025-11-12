7c3f4e1d853550df8cd398eb40ddc808230aa7f6
Andy Price <andyprice720@gmail.com>
2025-11-12 20:50:40 +0000

    feat(productDetails): create routes and view to update product details
    - Create productDetails view with form for editing product details,
      using forEach and conditional rendering to populate selects
    - Add getProductEditForm with hardcoded test data for verifying form
      display

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -27,6 +27,75 @@ async function getAllProducts(req, res) {
 	}
 }
 
+async function getProductEditForm(req, res) {
+	const productId = Number(req.params.id);
+
+	try {
+		// const product = await db.getProductById(productId);
+
+		// const [product, roasters, origins, processes, roasts]  = await Promise.all([
+		// 	db.getProductById(productId), 
+		// 	db.getAllRoasters(), 
+		// 	db.getAllOrigins(), 
+		// 	db.getAllProcesses(), 
+		// 	db.getAllRoasts(),
+		// ])
+
+		const product = [
+			{
+				id: 1,
+				name: "La Esperanza",
+				roaster_name: "Crank House Coffee",
+				roaster_country: "UK",
+				origin_country: "Guatemala",
+				process: "Washed",
+				variety: "Caturra, Bourbon, Catuai, Pache",
+				tasting_notes: "Gooseberry, red apple, caramel",
+				roast_style: "Light",
+				roast_type: "Filter",
+				price: 20,
+			},
+		];
+
+		const roasters = [
+			"Formative", "Crank House Coffee", "Full Court Press",
+		]
+
+		const origins = [
+			"Ethiopia", "Kenya", "Tanzania", "Rwanda", "Burundi", "Uganda", "Brazil", "Colombia", "Peru","Guatemala",
+		]
+
+		const processes = [
+			"Washed", "Natural", "Honey", "Anaerobic", "Carbonic Maceration"
+		]
+
+		const roasts = {
+			style: ["Light", "Medium", "Dark"],
+			type: ["Filter", "Espresso", "Omni"]
+		}
+
+		if (!product) {
+			res.status(404).send("Product not found");
+			return;
+		}
+
+		res.render("productDetails", { title: "Product Details", product, roasters, origins, processes, roasts });
+	} catch (err) {
+		console.error("Error loading product edit form:", err);
+		res.status(500).send("Server error");
+	}
+}
+
+async function postProductEditForm(req, res) {
+	// const product = await db.getProduceById(req.params.id)
+	console.log(req.params.id);
+	console.log(req.body);
+	
+	res.redirect("/products")
+}
+
 module.exports = {
 	getAllProducts,
+	getProductEditForm,
+	postProductEditForm,
 };
--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,7 @@ const productsRouter = Router();
 const productsController = require("../controllers/productsController");
 
 productsRouter.get("/", productsController.getAllProducts);
+productsRouter.get("/:id", productsController.getProductEditForm)
+productsRouter.post('/:id/update', productsController.postProductEditForm)
 
 module.exports = productsRouter;
